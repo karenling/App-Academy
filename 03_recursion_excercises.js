a2c7c08f45481137fe11a0b3e626aa272bdca74c
karenling <karensling@gmail.com>
2015-05-21 15:38:50 -0700

    implement sum_of_array

--- 03_recursion_excercises.js
+++ 03_recursion_excercises.js
@@ -9,6 +9,17 @@ function range(starting, ending) {
 }
 
 
-console.log(range(0, 10));
+// console.log(range(0, 10));
 
 // ---------------------------------------------------------------------
+
+function sum_of_array(array) {
+  if (array.length === 0) {
+    return 0
+  } else {
+    last_num = array.shift();
+    return last_num + sum_of_array(array); // why does order matter here?
+  }
+}
+
+console.log(sum_of_array([1, 2, 3]));
