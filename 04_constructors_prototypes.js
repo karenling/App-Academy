341484bd34529e788be408a03e66c752e09d081f
karenling <karensling@gmail.com>
2015-05-21 17:47:21 -0700

    create cats for constructors_prototype exercise

---
+++ 04_constructors_prototypes.js
@@ -0,0 +1,26 @@
+function Cat(name, owner) {
+  this.name = name;
+  this.owner = owner;
+}
+
+Cat.prototype.cuteStatement = function () {
+  return ("Everyone loves " + this.name + "!");
+};
+
+Cat.prototype.meow = function () {
+  return ("Meow!");
+};
+var gizmo = new Cat("Gizmo", "Ned");
+var breakfast = new Cat("Breakfast", "CJ");
+
+// console.log(gizmo.cuteStatement());
+// console.log(breakfast.cuteStatement());
+// console.log(gizmo.meow());
+// console.log(breakfast.meow());
+
+gizmo['meow'] = function () {
+  return ("Meowwwww!");
+};
+
+console.log(gizmo['meow']());
+console.log(breakfast['meow']());
