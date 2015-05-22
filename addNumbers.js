63d84953643973815bc9bf2e2c0c2d1a73b4ce88
Karen Ling <karensling@gmail.com>
2015-05-22 12:43:36 -0700

    complete addNumbers.js

--- addNumbers.js
+++ addNumbers.js
@@ -4,25 +4,55 @@ var reader = readline.createInterface({
   output: process.stdout
 });
 
-var addNumbers = function(sum, numsLeft, completionCallback){
-  if (numsLeft > 0)
-  {
+var addNumbers = function(sum, numsLeft, completionCallback) {
+
+  if (numsLeft === 0) {
+    completionCallback(sum);
+    reader.close();
+  } else {
     reader.question("Give me number", function(answer){
       var number = parseInt(answer);
       sum += number;
-      console.log("Current sum: " + sum);
+      console.log("Sum is: " + sum);
+      numsLeft--;
+      addNumbers(sum, numsLeft, completionCallback);
     });
-    numsLeft--;
-
-
-
-  }
-  else{
-    reader.close();
-    completionCallback(sum);
   }
 }
 
 addNumbers(0, 3, function (sum) {
   console.log("Total Sum: " + sum);
 });
+
+
+
+
+
+// var readline = require('readline');
+// var reader = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
+//
+// var addNumbers = function(sum, numsLeft, completionCallback){
+//   if (numsLeft > 0)
+//   {
+//     reader.question("Give me number", function(answer){
+//       var number = parseInt(answer);
+//       sum += number;
+//       console.log("Current sum: " + sum);
+//     });
+//     numsLeft--;
+//
+//
+//
+//   }
+//   else{
+//     reader.close();
+//     completionCallback(sum);
+//   }
+// }
+//
+// addNumbers(0, 3, function (sum) {
+//   console.log("Total Sum: " + sum);
+// });
