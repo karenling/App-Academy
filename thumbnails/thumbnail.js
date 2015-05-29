f54c50b704fa12990d40d527b47bb286fcf3d7bb
Karen Ling <karensling@gmail.com>
2015-05-28 17:54:34 -0700

    gutter images limit

--- thumbnails/thumbnail.js
+++ thumbnails/thumbnail.js
@@ -2,7 +2,12 @@ $.Thumbnail = function(el) {
   this.$el = $(el);
   this.$gutterImages =  $('.gutter-images');
   this.$activeImage = $('.gutter-images img:first-child');
+  this.gutterIdx = 0;
   this.activate(this.$activeImage);
+
+  this.$images = $('.gutter-images img');
+  this.fillGutterImages();
+
 
   //activate on click
   this.$gutterImages.on('click', 'img', function(event) {
@@ -25,7 +30,15 @@ $.Thumbnail.prototype.activate = function ($img) {
   $('.active img').remove();
   var imageURL = $img.attr("src");
   $('.active').append($('<img>').attr("src", imageURL));
-}
+};
+
+$.Thumbnail.prototype.fillGutterImages = function() {
+  $('.gutter-images img').remove();
+  for (var i = this.gutterIdx; i < this.gutterIdx + 5; i++) {
+    var imageURL = this.$images.eq(i).attr("src");
+    $('.gutter-images').append($('<img>').attr("src", imageURL));
+  }
+};
 
 $.fn.thumbnail = function () {
   return this.each(function () {
