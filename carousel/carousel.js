68e00e32094a2d8f242858c2b7ce7760992a055b
Karen Ling <karensling@gmail.com>
2015-05-28 17:08:46 -0700

    finished carousel

--- carousel/carousel.js
+++ carousel/carousel.js
@@ -1,24 +1,29 @@
 $.Carousel = function(el) {
   this.$el = $(el);
   this.activeIdx = 0;
-  this.removeTags.bind(this)();
-  this.addTags.bind(this)();
-  console.log("hi")
+  this.transitioning = false;
+  $('.items').eq(this.activeIdx).addClass("active");
+  // this.removeTags.bind(this)(0);
+  // this.addTags.bind(this)(0);
 
   $('.slide-left').on("click", this.slide.bind(this, -1));
   $('.slide-right').on("click", this.slide.bind(this, 1));
 };
 
 $.Carousel.prototype.slide = function (dir) {
+  if (this.transitioning) {
+    return;
+  }
+  this.transitioning = true;
   this.removeTags.bind(this)();
   this.activeIdx += dir;
-  this.addTags.bind(this)();
+  this.activeIdx = this.wrapIndex(this.activeIdx);
+  this.addTags.bind(this, dir)();
 };
 
 $.Carousel.prototype.wrapIndex = function (num) {
-  console.log("wrapping");
   var numItems = $('.items').length;
-  if (num > numItems) {
+  if (num > numItems - 1) {
     num -= numItems;
   } else if (num < 0) {
     num += numItems;
@@ -26,23 +31,41 @@ $.Carousel.prototype.wrapIndex = function (num) {
   return num;
 };
 
-$.Carousel.prototype.removeTags = function() {
-  console.log("removing");
-  $('.items').eq(this.activeIdx).removeClass('active');
-  $('.items').eq(this.wrapIndex(this.activeIdx + 1)).removeClass('active').removeClass('right');
-  $('.items').eq(this.wrapIndex(this.activeIdx - 1)).removeClass('active').removeClass('left');
+$.Carousel.prototype.removeTags = function(dir) {
+  var dirWord = "";
+  if (dir === -1) {
+    dirWord = "left";
+  } else if (dir === 1) {
+    dirWord = "right";
+  }
+
+  // $('.items').eq(this.activeIdx).removeClass('active');
+  // $('.items').eq(this.wrapIndex(this.activeIdx + 1)).removeClass('right');
+  // $('.items').eq(this.wrapIndex(this.activeIdx - 1)).removeClass('left');
+
+  var $currentImage = $('.items').eq(this.activeIdx).addClass(dirWord);
+  $currentImage.one("transitionend", function() {
+    $currentImage.removeClass("active left right");
+    this.transitioning = false;
+  }.bind(this));
+
 };
 
-$.Carousel.prototype.addTags = function() {
-  console.log("adding");
+$.Carousel.prototype.addTags = function(dir) {
 
-  $('.items').eq(this.activeIdx).addClass('active left');
-  // $('.items').eq(this.activeIdx).removeClass('left');
+  var dirWord = "";
+  if (dir === -1) {
+    dirWord = "left";
+  } else if (dir === 1) {
+    dirWord = "right";
+  }
+
+  $('.items').eq(this.activeIdx).addClass('active').addClass(dirWord);
   setTimeout(function() {
-    $('.items').eq(this.activeIdx).removeClass('left');
+    $('.items').eq(this.activeIdx).removeClass(dirWord);
   }.bind(this), 0);
-  $('.items').eq(this.wrapIndex(this.activeIdx + 1)).addClass('active right');
-  $('.items').eq(this.wrapIndex(this.activeIdx - 1)).addClass('active left');
+  $('.items').eq(this.wrapIndex(this.activeIdx + 1)).addClass('right');
+  $('.items').eq(this.wrapIndex(this.activeIdx - 1)).addClass('left');
   //active.Idx -> setTimeout, function removes 'left'
 };
 
