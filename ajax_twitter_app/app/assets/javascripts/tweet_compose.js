ba2a48e315a084f87372a242de2cd5604601a1b6
Karen Ling <karensling@gmail.com>
2015-05-29 17:35:08 -0700

    complete Phase IV: Tweetcompose Mentioned User

--- ajax_twitter_app/app/assets/javascripts/tweet_compose.js
+++ ajax_twitter_app/app/assets/javascripts/tweet_compose.js
@@ -2,14 +2,29 @@ $.TweetCompose = function(el) {
   this.$el = $(el);
   this.$inputs = $(':input');
   this.counter = 0;
+  this.$mentionedUserA = this.$el.find(".add-mentioned-user");
+  this.$mentionedUserA.on("click", this.addMentionedUser.bind(this));
   this.$el.find('textarea').on("input", this.remainingCount.bind(this));
   this.$el.on("submit", this.submit.bind(this));
+  $('.mentioned-users').on("click", 'a.remove-mentioned-user', this.removeMentionedUser.bind(this));
+
+
 };
 
+$.TweetCompose.prototype.removeMentionedUser = function(event) {
+  var currentLink = event.currentTarget;
+  var $parentDiv = $(currentLink.parentElement);
+  $parentDiv.hide();
+}
+$.TweetCompose.prototype.addMentionedUser = function() {
+  this.$selectMention = this.$el.find('script').html();
+  $('.mentioned-users').append(this.$selectMention);
+
+};
 $.TweetCompose.prototype.remainingCount = function() {
   this.counter++;
   $('.chars-left').html("Characters Remaining: " + (140-this.counter));
-}
+};
 $.TweetCompose.prototype.submit = function(event) {
   event.preventDefault();
   var formData = this.$el.serializeJSON();
@@ -31,6 +46,7 @@ $.TweetCompose.prototype.clearInput = function () {
   this.counter = 0;
   this.$el.find('textarea').val('');
   this.$el.find('select').val('');
+  $('.mentioned-users').empty();
 };
 
 $.TweetCompose.prototype.handleSuccess = function(response) {
