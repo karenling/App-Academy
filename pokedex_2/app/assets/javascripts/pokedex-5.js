6384cc50613fbb7ba41dc57bdcf372da608c78a8
Karen Ling <karensling@gmail.com>
2015-06-02 21:19:52 -0700

    start moving rootView. work on Pokedex.View.PokemonIndex

--- pokedex_2/app/assets/javascripts/pokedex-5.js
+++ pokedex_2/app/assets/javascripts/pokedex-5.js
@@ -5,15 +5,27 @@ Pokedex.Views.PokemonIndex = Backbone.View.extend({
   },
 
   initialize: function () {
+    this.collection = new Pokedex.Collections.Pokemon();
+    this.collection.fetch();
+    console.log(this.collection);
+    this.listenTo(this.collection, "sync", this.render.bind(this));
+    this.listenTo(this.collection, "add", this.addPokemonToList.bind(this));
   },
 
   addPokemonToList: function (pokemon) {
+    var pokemonContent = JST['pokemonListItem']({ pokemon: pokemon });
+    this.$el.append(pokemonContent);
   },
 
   refreshPokemon: function (options) {
   },
 
   render: function () {
+    this.$el.empty();
+
+    this.collection.each(function (pokemon) {
+      this.addPokemonToList(pokemon);
+    }.bind(this));
   },
 
   selectPokemonFromList: function (event) {
@@ -39,10 +51,9 @@ Pokedex.Views.ToyDetail = Backbone.View.extend({
   }
 });
 
-/*
+
 $(function () {
   var pokemonIndex = new Pokedex.Views.PokemonIndex();
   pokemonIndex.refreshPokemon();
   $("#pokedex .pokemon-list").html(pokemonIndex.$el);
 });
-*/
