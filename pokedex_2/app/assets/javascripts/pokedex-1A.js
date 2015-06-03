e075f5eca4d033b2b668dc8a283a0384a7182aa6
Karen Ling <karensling@gmail.com>
2015-06-02 20:56:01 -0700

    create pokemon-list-item-template and modify addPokemonToList

--- pokedex_2/app/assets/javascripts/pokedex-1A.js
+++ pokedex_2/app/assets/javascripts/pokedex-1A.js
@@ -1,13 +1,15 @@
 Pokedex.RootView.prototype.addPokemonToList = function (pokemon) {
-  var $li = $('<li class="poke-list-item">');
-  $li.data('id', pokemon.get('id'));
-
-  var shortInfo = ['name', 'poke_type'];
-  shortInfo.forEach(function (attr) {
-    $li.append(attr + ': ' + pokemon.get(attr) + '<br>');
-  });
-
-  this.$pokeList.append($li);
+  var content = JST['pokemonListItem']({ pokemon: pokemon });
+  this.$pokeList.append(content);
+  // var $li = $('<li class="poke-list-item">');
+  // $li.data('id', pokemon.get('id'));
+  //
+  // var shortInfo = ['name', 'poke_type'];
+  // shortInfo.forEach(function (attr) {
+  //   $li.append(attr + ': ' + pokemon.get(attr) + '<br>');
+  // });
+  //
+  // this.$pokeList.append($li);
 };
 
 Pokedex.RootView.prototype.refreshPokemon = function () {
