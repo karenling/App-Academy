c802149174aa02f7a5c5e3f30ae507908322b3a4
Karen Ling <karensling@gmail.com>
2015-06-04 13:57:26 -0700

    Feed show from index

--- news_reader_app/app/assets/javascripts/routers/router.js
+++ news_reader_app/app/assets/javascripts/routers/router.js
@@ -1,7 +1,8 @@
 NewsReader.Routers.Feeds = Backbone.Router.extend({
 
   routes: {
-    "": "index"
+    "": "index",
+    "feeds/:id": "feedShow"
   },
 
   initialize: function (options) {
@@ -13,5 +14,11 @@ NewsReader.Routers.Feeds = Backbone.Router.extend({
     this.feeds.fetch();
     var indexView = new NewsReader.Views.FeedsIndex({ collection: this.feeds });
     this.$rootEl.html(indexView.render().$el);
+  },
+
+  feedShow: function (id) {
+    var feed = this.feeds.getOrFetch(id);
+    var showView = new NewsReader.Views.FeedsShow({ model: feed });
+    this.$rootEl.html(showView.render().$el);
   }
 });
